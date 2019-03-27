c02c8f1faa870c3293566b5dedd05ad47e2e0acb
Emilie-D <emyss42016@gmail.com>
2019-03-27 13:57:48 +0100

    exercices ruby ok

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+#En ajouter un hashtag devant la ligne celle-ci devient un commentaire
---
+++ exo_04.rb
@@ -0,0 +1,3 @@
+puts "Salut, ça farte ?"
+
+#Le programme ne fonctionnait pas car il manquait un guillemet à la fin de la ligne
